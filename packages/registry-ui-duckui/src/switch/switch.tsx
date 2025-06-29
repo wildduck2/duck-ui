168e51d640731bc5bc1351723ab3034fe53a877c
abdo-spices <spices.digital@gmail.com>
2025-06-29 08:46:54 +0300

    hotfix(switch): adjust checked state translation

--- packages/registry-ui-duckui/src/switch/switch.tsx
+++ packages/registry-ui-duckui/src/switch/switch.tsx
@@ -13,7 +13,7 @@ function Switch({ className, checked, ref, 'aria-label': ariaLabel, ...props }: 
       ref={ref}
       className={cn(
         'appearance-none h-4 w-8 py-2 px-[0.5px] flex items-center justify-start transition-all rounded-full border bg-border border-border checked:bg-black checked:border-border ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 checked:border-primary',
-        'after:absolute relative after:bg-white after:rounded-full after:block checked:after:translate-x-[80%] after:size-4  after:duration-200 after:will-change-[width,transform] after:ease-(--duck-motion-ease) after:transition-all after:shadow',
+        'after:absolute relative after:bg-white after:rounded-full after:block checked:after:translate-x-[82%] after:size-4  after:duration-200 after:will-change-[width,transform] after:ease-(--duck-motion-ease) after:transition-all after:shadow',
         ' ',
         className,
       )}
