a9a87d3f13395e2eeab118fbab06fb06090b2fe0
wildduck2 <wezonaser50@gmail.com>
2025-09-02 18:58:51 +0300

    ignore

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
@@ -34,6 +35,10 @@ const nextConfig: NextConfig = {
         hostname: 'raw.githubusercontent.com',
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['~'] = path.resolve(__dirname)
+    return config
   },
 }
 
