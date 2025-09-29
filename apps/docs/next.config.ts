c83da034c2f672373d042eb747c02e8b739379f6
wildduck2 <wezonaser50@gmail.com>
2025-09-29 11:08:36 +0300

    ignore

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
