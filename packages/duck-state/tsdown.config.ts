cf4569dc2d38fc93c55d070edd60852720512596
wildduck2 <wezonaser50@gmail.com>
2026-01-08 22:26:43 +0200

    fix(duck-gen-docs): build  package.json error

--- packages/duck-state/tsdown.config.ts
+++ packages/duck-state/tsdown.config.ts
@@ -7,29 +7,3 @@ export default defineConfig({
   entry: ['./src/*/index.ts'],
   plugins: [],
 })
-
-// export default defineConfig({
-//   ...config,
-//   declaration: true,
-// })
-
-// import { defineConfig } from 'tsdown'
-//
-// export default defineConfig({
-//   rootDir: 'src',
-//   outDir: 'dist',
-//
-//   entry: ['src/**/*.{ts,tsx}', '!src/examples/**/*.{ts,tsx}'],
-//   declaration: true,
-//   clean: true,
-//
-//   // 👇 important to disable chunks
-//   splitting: false,
-//
-//   // 👇 optional: don't emit source maps
-//   sourceMap: false,
-//
-//   // 👇 optional: disables file name hashing
-//   treeshake: false, // optional if you want raw files
-//   minify: false, // prevents rollup/tsup-style minification
-// })
